9d8ff2052ca433da26d033b7a56867c579d93da1
Julio Cesar <118136237+JulioAraujo92@users.noreply.github.com>
2024-11-22 21:11:30 -0300

    Delete LAB III/my-first-rest-project directory

--- LAB III/my-first-rest-project/src/main/java/com/apiToDoList/demo/exception/DataViolation.java
+++
@@ -1,7 +0,0 @@
-package com.apiToDoList.demo.exception;
-
-public class DataViolation extends RuntimeException {
-    public DataViolation(String message) {
-        super(message);
-    }
-}
--- LAB III/my-first-rest-project/src/main/java/com/apiToDoList/demo/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.apiToDoList.demo.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
